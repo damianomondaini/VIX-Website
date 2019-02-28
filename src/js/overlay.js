6b080750ec4fe88d101161aed135f0d077a97a45
Damiano <damiano.mondaini@gmail.com>
2019-02-28 15:52:39 +0000

    Added animation on overlay

--- src/js/overlay.js
+++ src/js/overlay.js
@@ -1,7 +1,11 @@
 function openNav() {
-    document.getElementById("overlay").style.height = "100%";
-  }
+  document.querySelector('.main-header').classList.add("main-header__animation");
+  document.getElementById("overlay").style.height = "100%";
+  document.querySelector('.main-header').style.height = "100vh";
+}
   
   function closeNav() {
-    document.getElementById("overlay").style.height = "0%";
-  }
+    document.querySelector('.main-header').classList.remove("main-header__animation");
+    document.getElementById("overlay").style.height = "0";
+    document.querySelector('.main-header').style.height = "80px";
+}
