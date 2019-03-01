ec0a2f8342fc1b35f2813e5ab16fef7905309dee
Damiano <damiano.mondaini@gmail.com>
2019-03-01 12:48:03 +0000

    Fixed portfolio, header and fitText

--- src/js/fit-text.js
+++ src/js/fit-text.js
@@ -1,1 +1,3 @@
-window.fitText(document.querySelector('.landing__title'), 0.55);
+if (document.querySelector('.landing__title') != null) {
+    window.fitText(document.querySelector('.landing__title'), 0.55);    
+}
